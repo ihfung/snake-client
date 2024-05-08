18f0f2e84d459e62e502313c8b64d28997489f41
ihleung <ihfungleung@gmail.com>
2024-05-08 15:48:42 -0400

    updated

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
+    host: '165.227.47.243',
     port: 50541,
   });
 
--- play.js
+++ play.js
@@ -6,9 +6,13 @@ const setupInput = function () {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
 
+const handleUserInput = function(data) {
+  console.log(data);
+};
 console.log("Connecting ...");
 connect();
