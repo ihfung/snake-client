8d0c67aba6eff42bc04b1299e69d3d6b66df5b9f
ihleung <ihfungleung@gmail.com>
2024-05-08 10:21:44 -0400

    updated

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '10.0.0.152',
     port: 50541,
