81cf5fdd42186c0e300f0146e7d6c518d370753d
ihleung <ihfungleung@gmail.com>
2024-05-08 17:50:39 -0400

    updated

--- input.js
+++ input.js
@@ -2,17 +2,21 @@ let connection;
 
 const handleUserInput = function(keyInput) {
  
+  if (keyInput === 's') {
+    console.log("down");
+    connection.write('Move: down');
+  }
   if (keyInput === 'w') {
-    connection.write("Move: up");
+    console.log("up");
+    connection.write('Move: up');
   }
   if (keyInput === 'a') {
-    connection.write("Move: left");
-  }
-  if (keyInput === 's') {
-    connection.write("Move: down");
+    console.log("left");
+    connection.write('Move: left');
   }
   if (keyInput === 'd') {
-    connection.write("Move: right");
+    console.log("right");
+    connection.write('Move: right');
   }
   if (keyInput === '\u0003') {
     process.exit();
@@ -27,7 +31,7 @@ const setupInput = (conn) => {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
