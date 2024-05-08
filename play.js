9525f4669f87e57aed4209873c573a2c1536320b
ihleung <ihfungleung@gmail.com>
2024-05-08 16:56:46 -0400

    updated

--- play.js
+++ play.js
@@ -1,18 +1,7 @@
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-
-const handleUserInput = function(keyInput) {
-  if (keyInput === '\u0003') {
-    process.exit();
-  }
-  
-};
 console.log("Connecting ...");
+connect();
+
 setupInput();
